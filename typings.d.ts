4c0bc3c90c70e92d440d2e6959bbf619d0dab9d5
Nope <liu334275@gmail.com>
2021-06-10 22:50:52 +0800

    build: 图片 import 报错

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,11 @@
 declare module '*.module.sass';
 declare module '*.module.scss';
 declare module '*.module.css';
+declare module '*.png';
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
+}
